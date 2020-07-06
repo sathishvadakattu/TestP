4685e62b084d84d7ba3ac7aab381dc06334b8037
Sathish Vadakattu <sathish.vadakattu@solugenix.com>
2020-07-06 20:18:31 +0530

    ab

--- Test/ViewController.swift
+++ Test/ViewController.swift
@@ -14,6 +14,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         print("master")
+        print("")
         // Do any additional setup after loading the view.
     }
 
