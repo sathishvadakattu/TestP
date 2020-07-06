0405552b2473889b31547a4f03d65b208c19b164
Sathish Vadakattu <sathish.vadakattu@solugenix.com>
2020-07-06 19:46:56 +0530

    Initial Commit

---
+++ Test/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Test
+//
+//  Created by Solugenix India on 06/07/20.
+//  Copyright © 2020 Solugenix India. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
